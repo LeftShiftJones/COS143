60f9bcd36755bdfa76ba309f63873e5d66f17f93
LeftShiftJones <ryan_jones5@taylor.edu>
2018-01-17 14:15:33 -0500

    Added more things

--- AjaxPractice/login.js
+++ AjaxPractice/login.js
@@ -1,14 +1,36 @@
 function login(userName, password) {
     var ajax = new XMLHttpRequest();
-    var d = document.getElementById("messages");
     ajax.addEventListener("load",function() {
-        console.log("hi");
-        d.innerHTML = this.respons;
+        messages.innerHTML = this.response[0].message;
         console.dir(this.response);
+        if(this.response[0].status==true) {
+            document.getElementById("username").value="";
+            document.getElementById("password").value="";
+        }
     });
-    ajax.setResponseType="json";
+    ajax.responseType="json";
     ajax.open("POST","//cse.taylor.edu/~cos143/sessions.php");
     ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     var toSend = "name="+userName+"&password="+password;
+    ajax.send(toSend);
+}
+
+loginButton.addEventListener("click", function() {
+    login(username.value, password.value);
+});
+
+logoutButton.addEventListener("click", logout);
+
+function logout() {
+    var ajax = new XMLHttpRequest();
+    var d = document.getElementById("messages");
+    ajax.addEventListener("load",function() {
+        d.innerHTML = this.response[0].message;
+        console.dir(this.response);
+    });
+    ajax.responseType="json";
+    ajax.open("POST","//cse.taylor.edu/~cos143/sessions.php");
+    ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    var toSend = "method=delete";
     ajax.send(toSend);
 }
