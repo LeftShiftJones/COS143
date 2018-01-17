1bb6826d5a3d5d7c4cc0c13364afd269627fae59
LeftShiftJones <ryan_jones5@taylor.edu>
2018-01-17 13:15:23 -0500

    Deleted duplicate project folders

--- AjaxPractice/login.js
+++ AjaxPractice/login.js
@@ -1,12 +1,14 @@
 function login(userName, password) {
     var ajax = new XMLHttpRequest();
-    //run this when we get a response back
-    ajax.addEventListener("load", function() {
+    var d = document.getElementById("messages");
+    ajax.addEventListener("load",function() {
+        console.log("hi");
+        d.innerHTML = this.respons;
         console.dir(this.response);
-        document.getElementById("messages").innerHTML = this.response;
     });
-
-    ajax.open("post", "//cse.taylor.edu/~cos143/users.php");
-    ajax.send();
-
+    ajax.setResponseType="json";
+    ajax.open("POST","//cse.taylor.edu/~cos143/sessions.php");
+    ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    var toSend = "name="+userName+"&password="+password;
+    ajax.send(toSend);
 }
