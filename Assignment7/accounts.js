59bdd6581c9c3c514d9685c8f1dbee7e18ab3b78
LeftShiftJones <ryan_jones5@taylor.edu>
2018-01-18 08:59:03 -0500

    Changed items to fit rubric for assignment 7

--- Assignment7/accounts.js
+++ Assignment7/accounts.js
@@ -1,4 +1,48 @@
 /*
 Assignment 6's javascript file
 Handles Ajax protocols for tracking login/user information
+Ryan Jones | Thomas Taylor
 */
+
+/**
+Function to log users into the system
+*/
+function login(userName, password) {
+    var ajax = new XMLHttpRequest();
+    ajax.addEventListener("load",function() {
+        messages.innerHTML = this.response[0].message;
+        console.dir(this.response);
+        if(this.response[0].status==true) {
+            document.getElementById("username").value="";
+            document.getElementById("password").value="";
+        }
+    });
+    ajax.responseType="json";
+    ajax.open("POST","//cse.taylor.edu/~cos143/sessions.php");
+    ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    var toSend = "name="+userName+"&password="+password;
+    ajax.send(toSend);
+}
+
+/**
+Function to log users out of the system
+*/
+function logout() {
+    var ajax = new XMLHttpRequest();
+    var d = document.getElementById("messages");
+    ajax.addEventListener("load",function() {
+        d.innerHTML = this.response[0].message;
+        console.dir(this.response);
+    });
+    ajax.responseType="json";
+    ajax.open("POST","//cse.taylor.edu/~cos143/sessions.php");
+    ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    var toSend = "method=delete";
+    ajax.send(toSend);
+}
+
+loginButton.addEventListener("click", function() {
+    login(username.value, password.value);
+});
+
+logoutButton.addEventListener("click", logout);
